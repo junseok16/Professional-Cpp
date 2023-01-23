aa6f84259a49ac024e7ed18500b39f571b2f3619
Junseok Tak <tjsuk0220@gmail.com>
2023-01-23 16:34:59 +0900

    Init: Create SpreadsheetCell.h

---
+++ Professional-Cpp/Part 03. 전문가답게 C++ 코딩하기/Chapter 08. 클래스와 객체 숙달하기/SpreadsheetCell.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <string>
+#include <string_view>
+
+class SpreadsheetCell {
+public:
+	// 디폴트 생성자, 오버로드된 생성자
+	SpreadsheetCell();
+	SpreadsheetCell(double initialValue);
+	SpreadsheetCell(std::string_view initialString);
+
+	// 복사 생성자
+	SpreadsheetCell(const SpreadsheetCell& rhs);
+
+	// 복사 대입 연산자
+	SpreadsheetCell& operator=(const SpreadsheetCell& rhs);
+
+	void setValue(double inValue);
+	double getValue() const;
+
+	void setString(std::string_view inString);
+	std::string getString() const;
+
+private:
+	std::string doubleToString(double inValue) const;
+	double stringToDouble(std::string_view inString) const;
+	double mValue = 0;
+};
