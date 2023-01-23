6f716e0ddeeb4209ef304473cc55bfd7f7508f3b
Junseok Tak <tjsuk0220@gmail.com>
2023-01-23 16:34:37 +0900

    Init: Create main.cpp

---
+++ Professional-Cpp/Part 03. 전문가답게 C++ 코딩하기/Chapter 08. 클래스와 객체 숙달하기/main.cpp
@@ -0,0 +1,61 @@
+﻿#include "SpreadsheetCell.h"
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	// 스택에 객체를 생성합니다.
+	SpreadsheetCell firstCell, secondCell;
+	firstCell.setValue(1);
+	secondCell.setString("2.0");
+	cout << "The first cell is " << firstCell.getValue() << endl;
+	cout << "The second cell is " << secondCell.getValue() << endl;
+
+	// 힙에 객체를 생성합니다.
+	SpreadsheetCell* thirdCell = new SpreadsheetCell();
+	thirdCell->setValue(3.0);
+	cout << "The third cell is " << thirdCell->getValue() << endl;
+
+	delete thirdCell;
+	thirdCell = nullptr;
+
+	// 스마트 포인터로 힙에 객체를 생성합니다.
+	auto fourthCell = make_unique<SpreadsheetCell>();
+	fourthCell->setValue(4.0);
+	cout << "The fourth cell is " << fourthCell->getValue() << endl;
+
+
+
+	// 스택에 객체를 생성합니다.
+	SpreadsheetCell fifthCell(5);
+	cout << "The fifth cell is " << fifthCell.getValue() << endl;
+
+	// 힙에 객체를 생성합니다. 
+	SpreadsheetCell* sixthCell = new SpreadsheetCell(6);
+	cout << "The sixth cell is " << sixthCell->getValue() << endl;
+
+	delete sixthCell;
+	sixthCell = nullptr;
+
+	// 스마트 포인터로 힙에 객체를 생성합니다.
+	auto seventhCell = make_unique<SpreadsheetCell>(7);
+	cout << "The seventh cell is " << seventhCell->getValue() << endl;
+
+
+
+	// 스택에 객체를 생성합니다.
+	SpreadsheetCell eightCell("8");
+	cout << "The eight cell is " << eightCell.getValue() << endl;
+
+	// 힙에 객체를 생성합니다. 
+	SpreadsheetCell* ninethCell = new SpreadsheetCell("9.0");
+	cout << "The nineth cell is " << ninethCell->getValue() << endl;
+
+	delete ninethCell;
+	ninethCell = nullptr;
+
+	// 스마트 포인터로 힙에 객체를 생성합니다.
+	auto tenthCell = make_unique<SpreadsheetCell>("10");
+	cout << "The tenth cell is " << tenthCell->getValue() << endl;
+
+	return 0;
+}
